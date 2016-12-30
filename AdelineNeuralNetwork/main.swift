a0fcaacd30a9defa489a45d67a2be9d79b3b7f3a
Kenan Atmaca <kenanatmaca@Kenan-iMac.local>
2016-12-30 13:25:39 +0300

    Initial Commit

---
+++ AdelineNeuralNetwork/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdelineNeuralNetwork
+//
+//  Created by Kenan Atmaca on 30.12.2016.
+//  Copyright © 2016 Kenan Atmaca. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
